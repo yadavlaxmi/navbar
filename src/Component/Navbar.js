9a583c429e489ebcd14e2288c79e378f28e438e1
laxmi <laxmiyadav21@navgurukul.org>
2023-10-13 10:40:22 +0530

    create navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Grid } from "@mui/material";
-const Navbar=()=>{
+import React, { useState } from 'react'; 
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { AppBar, Grid, Typography,Toolbar,Tabs,Tab } from "@mui/material";
+import { ShoppingCartCheckout } from "@mui/icons-material";
+const Navbar=({links})=>{
+    const[val,setVal]=useState()
     return(
-        <>
-        <Grid container >
-            <Grid item xs={1} md={12}>hii</Grid>
-            <Grid item xs={1} md={12}>hello</Grid>
-        </Grid>
-        </>
+    
+        <AppBar>
+            <Toolbar>
+                <Grid container>
+                    <Grid item xs={2}>
+                        <Typography><ShoppingCartCheckout/></Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <Tabs indicatorColor="secondary" textColor="inherit" value={val} onChange={(e,val)=>setVal(val)}>
+                            <Tab label="product"></Tab>
+                            <Tab label="overview"></Tab>setVal
+                            <Tab label="pricing"></Tab>
+
+                        </Tabs>
+                    </Grid>
+
+                </Grid>
+            </Toolbar>
+               
+        </AppBar>
+        
     )
 }
 export default Navbar
