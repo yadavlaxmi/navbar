6f8125166ef84b744e7a5189f5d84678436acefd
laxmi <laxmiyadav21@navgurukul.org>
2023-10-13 14:39:08 +0530

    use mediaquery and put condition

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,44 +1,72 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { useMediaQuery, useTheme } from '@mui/material';
+
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import DrawerComp from './DrawerComp';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import {useMediaQuery,placeItems, marginLeft,AppBar, Grid, Typography,Toolbar,Tabs,Tab, Button,Box } from "@mui/material";
-import { ShoppingCartCheckout } from "@mui/icons-material";
-import { useTheme } from '@emotion/react';
-const Navbar=({links})=>{
-    const theme=useTheme();
-    console.log(theme)
 
-    const[val,setVal]=useState()
-    return(
-    
-        <AppBar sx={{backgroundImage:"linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,0.9024390243902439) 35%, rgba(0,212,255,1) 100%);"}}>
-            <Toolbar>
-                <Grid sx={{placeItems:"center"}} container>
-                    <Grid item xs={2}>
-                        <Typography><ShoppingCartCheckout/></Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Tabs indicatorColor="secondary" textColor="inherit" value={val} onChange={(e,val)=>setVal(val)}>
-                            {links.map((link,index)=>(
-                                <Tab key={index} label={link}/>
-                            ))}
+const Navbar = ({ links }) => {
+  const theme = useTheme();
+  console.log(theme)
+  const isMatch = useMediaQuery(theme.breakpoints.down('sm'));
+  console.log(isMatch)
+  const [val, setVal] = useState(0); 
 
-                        </Tabs>
-                    </Grid>
-                   <Grid item xs={1}/>
-                   <Grid item xs={3}>
-                    <Box display={'flex'}>
-                        <Button sx={{marginLeft:"auto",background:"rgba(180,58,58,1)"}}variant='contained'>Login</Button>
-                        <Button sx={{marginLeft:1,background:"rgba(180,58,58,1)"}}variant='contained'>sign up</Button>
+  return (
+    <AppBar
+      sx={{
+        backgroundImage: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,0.9024390243902439) 35%, rgba(0,212,255,1) 100%)"
+      }}
+    >
+      <Toolbar>
+        {isMatch}
+        <Grid sx={{ placeItems: 'center' }} container>
+          <Grid item xs={2}>
+            <Typography>
+              <ShoppingCartCheckoutIcon /> {/* Use the icon component */}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Tabs
+              indicatorColor="secondary"
+              textColor="inherit"
+              value={val}
+              onChange={(e, val) => setVal(val)}
+            >
+              {links.map((link, index) => (
+                <Tab key={index} label={link} />
+              ))}
+            </Tabs>
+          </Grid>
+          <Grid item xs={1} />
+          <Grid item xs={3}>
+            <Box display="flex">
+              <Button
+                sx={{ marginLeft: 'auto', background: 'rgba(180,58,58,1)' }}
+                variant="contained"
+              >
+                Login
+              </Button>
+              <Button
+                sx={{ marginLeft: 1, background: 'rgba(180,58,58,1)' }}
+                variant="contained"
+              >
+                Sign up
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+        <DrawerComp />
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-                    </Box>
-                    </Grid>
-                </Grid>
-                <DrawerComp/>
-            </Toolbar>
-               
-        </AppBar>
-        
-    )
-}
-export default Navbar
+export default Navbar;
