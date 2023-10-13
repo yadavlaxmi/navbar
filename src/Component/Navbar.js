69dc5831128c82f3ae8ca049f81430932e6ea85d
laxmi <laxmiyadav21@navgurukul.org>
2023-10-13 11:17:26 +0530

    create navbar make button of login and signup

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'; 
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { AppBar, Grid, Typography,Toolbar,Tabs,Tab } from "@mui/material";
+import { marginLeft,AppBar, Grid, Typography,Toolbar,Tabs,Tab, Button,Box } from "@mui/material";
 import { ShoppingCartCheckout } from "@mui/icons-material";
 const Navbar=({links})=>{
     const[val,setVal]=useState()
@@ -12,7 +12,7 @@ const Navbar=({links})=>{
                     <Grid item xs={2}>
                         <Typography><ShoppingCartCheckout/></Typography>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={6}>
                         <Tabs indicatorColor="secondary" textColor="inherit" value={val} onChange={(e,val)=>setVal(val)}>
                             {links.map((link,index)=>(
                                 <Tab key={index} label={link}/>
@@ -20,7 +20,14 @@ const Navbar=({links})=>{
 
                         </Tabs>
                     </Grid>
+                   <Grid item xs={1}/>
+                   <Grid item xs={3}>
+                    <Box display={'flex'}>
+                        <Button sx={{marginLeft:"auto"}}variant='contained'>Login</Button>
+                        <Button sx={{marginLeft:"1"}}variant='contained'>sign up</Button>
 
+                    </Box>
+                    </Grid>
                 </Grid>
             </Toolbar>
                
