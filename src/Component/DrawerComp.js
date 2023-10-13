81e3eabcf239edccd1670d1ece6ba7320596226a
laxmi <laxmiyadav21@navgurukul.org>
2023-10-13 16:12:26 +0530

    find menu list

--- src/Component/DrawerComp.js
+++ src/Component/DrawerComp.js
@@ -1,27 +1,35 @@
-import React, { useState } from 'react'; 
-
-import { List,Drawer, IconButton, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import React, { useState } from 'react';
+import {
+  List,
+  Drawer,
+  IconButton,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
+const DrawerComp = ({ links }) => {
+  const [open, setOpen] = useState(false);
 
-const DrawerComp=(links)=>{
-    const[open,setOpen]=useState(false)
-    return(
-        <>
-        <Drawer   open={open} onClose={()=>setOpen(false)}>
-            <List>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>Products</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-            </List>
-        </Drawer>
-        <IconButton sx={{marginLeft:"auto", color:"white"}}onClick={()=>setOpen(!open)}>
-            <MenuRoundedIcon/>
-        </IconButton>
-        </>
+  return (
+    <>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List>
+          {links.map((link, index) => (
+            <ListItemButton key={index}>
+              <ListItemIcon>
+                <ListItemText primary={link} />
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton sx={{ marginLeft: "auto", color: "white" }} onClick={() => setOpen(!open)}>
+        <MenuRoundedIcon />
+      </IconButton>
+    </>
+  );
+};
 
-    )
-}
 export default DrawerComp;
